88db1bf493494b8b5410fa985db4edee9095cabb
ericmegalovania <1486853661@qq.com>
2025-12-30 16:27:36 +0800

    revise control_hazard logic

--- pipeline/src/pipelined_cpu.v
+++ pipeline/src/pipelined_cpu.v
@@ -132,7 +132,8 @@ module pipelined_cpu(
                              (id_ex_write_reg_addr == if_id_inst[`RT]));
     
     // 控制冒险：分支/跳转指令
-    assign control_hazard = id_ex_valid && (id_ex_branch_flag || id_ex_jump_flag);
+    assign control_hazard = (id_ex_valid && (id_ex_branch_flag || id_ex_jump_flag)) ||
+                            (ex_mem_valid && (ex_mem_branch_flag || ex_mem_jump_flag));
     
     // 流水线控制信号
     always @(*) begin
